9c3b12cfce5b3305d0f818842ba4f782bad8885c
maatoug610 <email>
2022-02-17 12:42:28 +0100

    fdd

---
+++ Assets/Scripts/SystemRewards/GameData.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public static class GameData {
+	
+	private static int _coins = 0;
+	private static int _gems = 0;
+
+	//Static Constructor to load data from playerPrefs
+	static GameData ( ) {
+		
+		_coins = PlayerPrefs.GetInt ( "Coins", 0 );
+		_gems = PlayerPrefs.GetInt ( "Gems", 0 );
+	}
+
+	
+	public static int Coins {
+		get{ return _coins; }
+		set{ PlayerPrefs.SetInt ( "Coins", (_coins = value) ); }
+	}
+
+	public static int Gems {
+		get{ return _gems; }
+		set{ PlayerPrefs.SetInt ( "Gems", (_gems = value) ); }
+	}
+
+	/*---------------------------------------------------------
+		this line:
+		set{ PlayerPrefs.SetInt ( "Gems", (_gems = value) ); }
+		is equivalent to:
+		set{ 
+			_gems = value;
+			PlayerPrefs.SetInt ( "Gems", _gems ); 
+		}
+	------------------------------------------------------------*/
+}
